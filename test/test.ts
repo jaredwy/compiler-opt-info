76f2aabad8cb4fe7b06f316c2d91c8fe5d02019b
[aviv polak] <[avivp0437@gmail.com]>
2022-02-01 14:03:29 +0200

    added \n to docEnd.

--- test/test.ts
+++ test/test.ts
@@ -5,76 +5,76 @@ import { DisplayOptInfo } from "../src/OptInformation";
 import { createReadStream, readdirSync } from "fs";
 import { count } from "console";
 
-// test.cb("test a simple result", (t) => {
-//     let rec = 1;
-//     createReadStream("test/resources/test.yaml", { encoding: "utf-8" })
-//         .pipe(new LLVMOptTransformer({}))
-//         .on("data", (x: LLVMOptInfo) => {
-//             if (rec == 1) {
-//                 t.is(x.Name, "NoDefinition");
-//                 t.is(x.optType, "Missed");
-//                 t.is(
-//                     DisplayOptInfo(x),
-//                     "realloc will not be inlined into _Z13record_resultdPKc because its definition is unavailable"
-//                 );
-//                 t.is(x.displayString, DisplayOptInfo(x));
-//                 rec++;
-//             } else if (rec == 2) {
-//                 t.is(x.Name, "NotInlined");
-//                 t.is(x.optType, "Missed");
-//                 t.end();
-//             }
-//         });
-// });
+test.cb("test a simple result", (t) => {
+    let rec = 1;
+    createReadStream("test/resources/test.yaml", { encoding: "utf-8" })
+        .pipe(new LLVMOptTransformer({}))
+        .on("data", (x: LLVMOptInfo) => {
+            if (rec == 1) {
+                t.is(x.Name, "NoDefinition");
+                t.is(x.optType, "Missed");
+                t.is(
+                    DisplayOptInfo(x),
+                    "realloc will not be inlined into _Z13record_resultdPKc because its definition is unavailable"
+                );
+                t.is(x.displayString, DisplayOptInfo(x));
+                rec++;
+            } else if (rec == 2) {
+                t.is(x.Name, "NotInlined");
+                t.is(x.optType, "Missed");
+                t.end();
+            }
+        });
+});
 
-// test.cb("ensure large file has correct count", (t) => {
-//     let count = 0;
-//     createReadStream("test/resources/loop_unroll.opt.yaml", {
-//         encoding: "utf-8",
-//     })
-//         .pipe(new LLVMOptTransformer({}))
-//         .on("data", (x: LLVMOptInfo) => count++)
-//         .on("end", () => {
-//             t.is(count, 996);
-//             t.end();
-//         });
-// });
+test.cb("ensure large file has correct count", (t) => {
+    let count = 0;
+    createReadStream("test/resources/loop_unroll.opt.yaml", {
+        encoding: "utf-8",
+    })
+        .pipe(new LLVMOptTransformer({}))
+        .on("data", (x: LLVMOptInfo) => {count++})
+        .on("end", () => {
+            t.is(count, 996);
+            t.end();
+        });
+});
 
-// test.cb("ensure that pass and analysis are accounted for", (t) => {
-//     let count = 0;
-//     createReadStream("test/resources/O3.opt.yaml", { encoding: "utf-8" })
-//         .pipe(new LLVMOptTransformer({}))
-//         .on("data", (x: LLVMOptInfo) => {
-//             if (count == 0) {
-//                 t.is(x.optType, "Analysis");
-//                 t.is(
-//                     DisplayOptInfo(x),
-//                     "_Z10accumulateiPi can be inlined into _Z10computesumPii with cost=-35 (threshold=375)"
-//                 );
-//             }
-//             if (count == 1) {
-//                 t.is(x.optType, "Passed");
-//                 t.is(
-//                     DisplayOptInfo(x),
-//                     "_Z10accumulateiPi inlined into _Z10computesumPii"
-//                 );
-//             } else if (count == 2) {
-//                 t.is(x.optType, "Passed");
-//                 t.is(DisplayOptInfo(x), "hosting zext");
-//             }
-//             count++;
-//         })
-//         .on("end", () => {
-//             t.end();
-//         });
-// });
+test.cb("ensure that pass and analysis are accounted for", (t) => {
+    let count = 0;
+    createReadStream("test/resources/O3.opt.yaml", { encoding: "utf-8" })
+        .pipe(new LLVMOptTransformer({}))
+        .on("data", (x: LLVMOptInfo) => {
+            if (count == 0) {
+                t.is(x.optType, "Analysis");
+                t.is(
+                    DisplayOptInfo(x),
+                    "_Z10accumulateiPi can be inlined into _Z10computesumPii with cost=-35 (threshold=375)"
+                );
+            }
+            if (count == 1) {
+                t.is(x.optType, "Passed");
+                t.is(
+                    DisplayOptInfo(x),
+                    "_Z10accumulateiPi inlined into _Z10computesumPii"
+                );
+            } else if (count == 2) {
+                t.is(x.optType, "Passed");
+                t.is(DisplayOptInfo(x), "hosting zext");
+            }
+            count++;
+        })
+        .on("end", () => {
+            t.end();
+        });
+});
 
 test("the system dosn't crush with a lot of yaml files", async (t) => {
     //for some reason the test is still pass even though the crush is happening,
     // but in this case the error is still thrown
-    const files = readdirSync("test/resources/sample_yamls");
+    const files = readdirSync("test/resources/");
     try {
-        await crashTest(files, "test/resources/sample_yamls/");
+        await crashTest(files, "test/resources/");
         t.pass();
     } catch (error) {
         t.fail();
